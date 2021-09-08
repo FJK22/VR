abb519ca9de35903c37c896a9f14e607bcd73e77
Fjorda Kazazi <79405767+FJK22@users.noreply.github.com>
2021-09-08 17:06:20 +0100

    Sc2BLectureHall Scene Added

--- Assets/Scripts/Level2/Sc2Question.cs
+++ Assets/Scripts/Level2/Sc2Question.cs
@@ -12,13 +12,19 @@ public class Sc2Question : MonoBehaviour
     string _q2 = "";
     string _q3 = "";
     string _q4 = "";
+    string _q5 = "";
+    string _q6 = "";
+
     public string Q1 { set { _q1 = value; Validate(); } }
     public string Q2 { set { _q2 = value; Validate(); } }
     public string Q3 { set { _q3 = value; Validate(); } }
     public string Q4 { set { _q4 = value; Validate(); } }
+    public string Q5 { set { _q5 = value; Validate(); } }
+    public string Q6 { set { _q6 = value; Validate(); } }
+
     void Validate()
     {
-        BtSubmit.interactable = _q1 != "" && _q2 != "" && _q3 != "" && _q4 != "";
+        BtSubmit.interactable = _q1 != "" && _q2 != "" && _q3 != "" && _q4 != "" && _q5 != "" && _q6 != "";
     }
     public void Submit()
     {
@@ -34,6 +40,9 @@ public class Sc2Question : MonoBehaviour
         formData.Add(new MultipartFormDataSection("q2", _q2));
         formData.Add(new MultipartFormDataSection("q3", _q3));
         formData.Add(new MultipartFormDataSection("q4", _q4));
+        formData.Add(new MultipartFormDataSection("q5", _q5));
+        formData.Add(new MultipartFormDataSection("q6", _q6));
+
         UnityWebRequest www = UnityWebRequest.Post(Constant.DOMAIN + Constant.SC2QS, formData);
         yield return www.SendWebRequest();
         if (www.result != UnityWebRequest.Result.Success)
